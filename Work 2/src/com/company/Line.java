d0a68e70c142f0b0f350ac50eb621f8006846802
almaznur91 <almazic91@gmail.com>
2017-10-02 23:01:40 +0300

    work2_unfinished

---
+++ Work 2/src/com/company/Line.java
@@ -0,0 +1,18 @@
+package com.company;
+
+public class Line {
+
+    public int k;
+    public int b;
+
+
+    public Line(int k, int b) {
+        this.b = b;
+        this.k = k;
+    }
+    public static int f(int x) {
+        return k*x+b;
+    }
+
+
+}
