cb74172f0739de18bb12bee0a02352909b6406a6
zagirnur <zagirnur@gmail.com>
2017-10-04 22:13:33 +0300

    Тут задача одна

--- work4/src/com/company/Main.java
+++ work4/src/com/company/Main.java
@@ -3,7 +3,12 @@ package com.company;
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+	    //TODO: Создай классы Cat и Human
+        //TODO: Создай женщину и двух кошек
+        //TODO: У Cat поля: Human mistress; String name; int age;
+        //TODO: У Cat функции: сказать мяу;
+        //TODO: У Human поля: int Age; String name; Cat[] cats;
+        //TODO: У Human функции: сколько кошек у человека
 
     }
 }
