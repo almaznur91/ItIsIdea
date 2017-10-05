8dae7557ae140fe7f49f14672065b57928c1815c
zagirnur <zagirnur@gmail.com>
2017-10-05 09:59:33 +0300

    Вторая папка

--- work4/src/com/company/Main2.java
+++ work4/src/com/company/Main2.java
@@ -3,5 +3,6 @@ package com.company;
 public class Main2 {
     public static void main(String[] args) {
         System.out.println("йееее");
+        System.out.println("Работа из разных папок");
     }
 }
