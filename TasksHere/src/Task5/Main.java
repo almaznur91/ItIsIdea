721344d9be9d21682c1a015607b4310484dbb636
zagirnur <zagirnur@gmail.com>
2017-10-05 12:15:58 +0300

    5 задача

--- TasksHere/src/Task5/Main.java
+++ TasksHere/src/Task5/Main.java
@@ -2,6 +2,10 @@ package Task5;
 
 public class Main {
     public static void main(String[] args) {
-        
+        //TODO: Класс Кошка создай
+        //TODO: массив из кошек.
+        //TODO: создай две кошки
+        //TODO: занеси в массив эти кошки
+        //TODO: выведи на экран количество кошек в массиве
     }
 }
