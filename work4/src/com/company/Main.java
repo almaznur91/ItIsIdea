3ffca6e5e33c167486de9a7b2c6000b323b083d0
zagirnur <zagirnur@gmail.com>
2017-10-05 09:22:54 +0300

    пронумеровал задания)

--- work4/src/com/company/Main.java
+++ work4/src/com/company/Main.java
@@ -3,12 +3,12 @@ package com.company;
 public class Main {
 
     public static void main(String[] args) {
-	    //TODO: Создай классы Cat и Human
-        //TODO: Создай женщину и двух кошек
-        //TODO: У Cat поля: Human mistress; String name; int age;
-        //TODO: У Cat функции: сказать мяу;
-        //TODO: У Human поля: int Age; String name; Cat[] cats;
-        //TODO: У Human функции: сколько кошек у человека
+	    //TODO: 1)Создай классы Cat и Human
+        //TODO: 2)Создай женщину и двух кошек
+        //TODO: 3)У Cat поля: Human mistress; String name; int age;
+        //TODO: 4)У Cat функции: сказать мяу;
+        //TODO: 5)У Human поля: int Age; String name; Cat[] cats;
+        //TODO: 6)У Human функции: сколько кошек у человека
 
     }
 }
