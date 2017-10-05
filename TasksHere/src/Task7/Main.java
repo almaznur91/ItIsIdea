162c552b869ed6061401207871a10110e213bf7c
zagirnur <zagirnur@gmail.com>
2017-10-05 14:31:02 +0300

    Задача

---
+++ TasksHere/src/Task7/Main.java
@@ -0,0 +1,22 @@
+package Task7;
+
+public class Main {
+    /**
+     * НИ НАМЕКА НА ООП=)
+     */
+    public static void main(String[] args) {
+        String[] string=new String[10];
+        for (int i=0;i<string.length;i++){
+            string[i]="Номер "+i+"1, ";
+        }
+        //выше я создал массив строк и заполнил их строками
+        //TODO: сделай функцию чтобы поменять местами значения двух элементов массива
+        //TODO: swap(4,6); вот так например
+        //эта функция прямо в этом классе
+
+        //TODO: Bonus: выведи массив на экран через переменную
+        String myStr="";
+        //тут внесение массива в переменную
+        System.out.println(myStr);
+    }
+}
