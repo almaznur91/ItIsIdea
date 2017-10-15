30108bda847db404f31958cb938da47ac442b0bb
almaznur91 <almazic91@gmail.com>
2017-10-15 20:52:32 +0300

    JR

---
+++ JavaRush/inheritance/src/com/company/Cat.java
@@ -0,0 +1,15 @@
+package com.company;
+
+public class Cat {
+
+    static String multiply(String s, int count){
+        String result = "";
+        for (int i=0;i<=count;i++){
+
+            result = s+ result ;
+
+        }
+        return result;
+    }
+
+}
--- JavaRush/inheritance/src/com/company/Main.java
+++ JavaRush/inheritance/src/com/company/Main.java
@@ -7,25 +7,8 @@ public class Main {
 
 
     public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
-        int n = scanner.nextInt();
-        int num = 1;
-        if (n > 0) {
-            num = n;
-        } else {
-            System.out.println("введите значение более 0");
-        }
-        int[] array= new int[num];
-        for (int i=0;i<array.length;i++){
-            array[i]=scanner.nextInt();
-        }
-        int max=array[0];
-        for (int i=0;i<array.length;i++){
-            if (max<array[i]){
-                max=array[i];
-            }
-        }
-        System.out.println(max);
+     Cat cat = new Cat();
+        System.out.println(Cat.multiply("fff",8));
 
     }
 }
