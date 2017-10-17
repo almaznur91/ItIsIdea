32cdae604382cd1f6fda88af737adfdc1d100d9b
almaznur91 <32224755+almaznur91@users.noreply.github.com>
2017-10-17 16:27:33 +0300

    JR

--- JavaRush/inheritance/src/com/company/Main.java
+++ JavaRush/inheritance/src/com/company/Main.java
@@ -9,15 +9,26 @@ public class Main {
 
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
+        int n = scanner.nextInt();
+        int m = scanner.nextInt();
         ArrayList<String> list = new ArrayList<String>();
-        for (int i = 0; i < 5; i++) {
+        for (int i = 0; i < n; i++) {
             list.add(scanner.nextLine());
         }
-        list.remove(2);
-
-        for (int i=0;i<list.size();i++){
-            System.out.println(list.get(i));
+        for (int i = 0; i < m; i++) {
+            list.add(list.size() - m + i, list.get(i));
         }
-
+        for (int i = 0; i < list.size(); i++) {
+            System.out.print(list.get(i));
+        }
     }
+
+    public static ArrayList<String> doubleValues(ArrayList<String> list) {
+        for (int i = 0; i < list.size(); i++) {
+            list.add(i + 1, list.get(i));
+            i++;
+        }
+        return list;
+    }
+
 }
