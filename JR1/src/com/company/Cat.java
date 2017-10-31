02c570b82952cb47a705f4654802f707b19f73d7
almaznur91 <almazic91@gmail.com>
2017-10-31 22:36:53 +0300

    JR

---
+++ JR1/src/com/company/Cat.java
@@ -0,0 +1,13 @@
+package com.company;
+
+public class Cat {
+    public Cat(String name) {
+        this.name = name;
+    }
+
+    public String name;
+
+    public String toString() {
+return "Cat"+this.name;
+    }
+    }
--- JR1/src/com/company/Main.java
+++ JR1/src/com/company/Main.java
@@ -1,8 +1,38 @@
 package com.company;
 
+import java.util.*;
+
 public class Main {
+/*
+    Создать класс кот – Cat, с полем «имя» (String).
+    Создать словарь Map(<String, Cat>) и добавить туда 10 котов в виде «Имя»-«Кот».
+    Получить из Map множество(Set) всех имен и вывести его на экран.*/
 
     public static void main(String[] args) {
-	// write your code here
+
+        Map<String, Cat> map = new HashMap<>();
+
+
+        Cat cat1 = new Cat("vaska");
+        map.put("vaska", cat1);
+
+        Set<Cat> set = convertMaptoSet(map);
+        for (Cat v : set) {
+            System.out.println(v);
+        }
     }
+
+    public static Set<Cat> convertMaptoSet(Map<String, Cat> map) {
+        Set<Cat> set = new HashSet<>();
+
+        Iterator<Map.Entry<String, Cat>> iterator = map.entrySet().iterator();
+        while (iterator.hasNext()) {
+            Map.Entry<String, Cat> pair = iterator.next();
+            set.add(pair.getValue());
+
+        }
+        return set;
+    }
+
 }
+
