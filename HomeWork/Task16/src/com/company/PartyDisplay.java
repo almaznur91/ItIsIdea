09823d0dea7f4378413e254cd38949bda2f08dc5
almaznur91 <32224755+almaznur91@users.noreply.github.com>
2017-11-21 19:32:51 +0300

    0.003003

--- HomeWork/Task16/src/com/company/PartyDisplay.java
+++ HomeWork/Task16/src/com/company/PartyDisplay.java
@@ -1,6 +1,6 @@
 package com.company;
 
-public class ForecastDisplay implements  Observer {
+public class PartyDisplay implements  Observer {
 
         public void handleEvent() {
             System.out.println("А князь то не настоящий!");
