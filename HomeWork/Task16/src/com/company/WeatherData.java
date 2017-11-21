d19dd0a538eb9af07f19e218ef0bc986f08ab75a
almaznur91 <32224755+almaznur91@users.noreply.github.com>
2017-11-21 18:04:28 +0300

    0.1

--- HomeWork/Task16/src/com/company/WeatherData.java
+++ HomeWork/Task16/src/com/company/WeatherData.java
@@ -5,11 +5,14 @@ import java.util.Observable;
 public class WeatherData  implements com.company.Observable{
     StatisticDisplay statisticDisplay;
 
-
+    void setObserver(Observer observer){
+    statisticDisplay=observer;  //чет не прет
+    }
+    
     @Override
     public void event(Observer observer) {
          {
-            observer.handleEvent();
+            statisticDisplay.handleEvent();
         }
     }
 }
